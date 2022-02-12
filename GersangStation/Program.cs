b4067bd59e4ccefdcb64c919af5a377d10699d9a
byungmeo <wlsrlfvkr@gmail.com>
2022-02-12 14:13:15 +0900

    중복 실행 방지

--- GersangStation/Program.cs
+++ GersangStation/Program.cs
@@ -5,16 +5,24 @@ namespace GersangStation {
         /// </summary>
         [STAThread]
         static void Main() {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
+            bool flagMutex; //중복 실행 여부 확인
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            //Application.SetHighDpiMode(HighDpiMode.SystemAware); -> 100% 보다 높은 DPI 사용 시 인터페이스 위치가 깨짐
-            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
+            Mutex mutex = new Mutex(true, "GersangClientStation", out flagMutex);
+            if (flagMutex) {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                //ApplicationConfiguration.Initialize();
 
-            Application.Run(new Form1());
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                //Application.SetHighDpiMode(HighDpiMode.SystemAware); -> 100% 보다 높은 DPI 사용 시 인터페이스 위치가 깨짐
+                Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
+
+                Application.Run(new Form1());
+                mutex.ReleaseMutex();
+            } else {
+                MessageBox.Show("중복 실행은 불가능 합니다.", "중복 실행", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
