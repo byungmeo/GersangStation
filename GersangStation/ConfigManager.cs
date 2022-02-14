2177b53aa051c9e146a63e605a17c3b86d51c6a0
byungmeo <wlsrlfvkr@gmail.com>
2022-02-14 18:29:54 +0900

    config 설정 누락 시 추가

--- GersangStation/ConfigManager.cs
+++ GersangStation/ConfigManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Configuration;
+using System.Diagnostics;
 using System.Reflection;
 using System.Text;
 
@@ -7,40 +8,49 @@ namespace GersangStation {
         public static Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
         public static void Validation() {
-            if (false == ExistsConfig()) {
-                CreateConfigFile();
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs.Add("current_preset", "1");
+            keyValuePairs.Add("account_list", "");
+            keyValuePairs.Add("current_comboBox_index_preset_1", "0;0;0");
+            keyValuePairs.Add("current_comboBox_index_preset_2", "0;0;0");
+            keyValuePairs.Add("current_comboBox_index_preset_3", "0;0;0");
+            keyValuePairs.Add("current_comboBox_index_preset_4", "0;0;0");
+            keyValuePairs.Add("is_test_server", "False");
+            keyValuePairs.Add("client_path_1", "");
+            keyValuePairs.Add("client_path_2", "");
+            keyValuePairs.Add("client_path_3", "");
+            keyValuePairs.Add("client_path_test_1", "");
+            keyValuePairs.Add("client_path_test_2", "");
+            keyValuePairs.Add("client_path_test_3", "");
+            keyValuePairs.Add("shortcut_name", "거상홈페이지;홈페이지2;홈페이지3;홈페이지4;");
+            keyValuePairs.Add("shortcut_1", "https://www.gersang.co.kr/main/index.gs");
+            keyValuePairs.Add("shortcut_2", "");
+            keyValuePairs.Add("shortcut_3", "");
+            keyValuePairs.Add("shortcut_4", "");
+            keyValuePairs.Add("directory_name_client_2", "Gersang2");
+            keyValuePairs.Add("directory_name_client_3", "Gersang3");
+            keyValuePairs.Add("directory_name_client_test_2", "GerTest2");
+            keyValuePairs.Add("directory_name_client_test_3", "GerTest3");
+            keyValuePairs.Add("is_auto_update", "True");
+
+            if (false == ExistsConfig()) {  CreateConfigFile(keyValuePairs);  }
+            else { CheckKey(keyValuePairs); }
+        }
+
+        private static void CheckKey(Dictionary<string, string> keyValuePairs) {
+            foreach (var item in keyValuePairs) {
+                KeyValueConfigurationElement element = configuration.AppSettings.Settings[item.Key];
+                if (element == null) { addConfig(item.Key, item.Value); }
             }
         }
 
-        private static void CreateConfigFile() {
+        private static void CreateConfigFile(Dictionary<string,string> keyValuePairs) {
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
             sb.AppendLine("<configuration>");
             sb.AppendLine("<appSettings>");
 
-            AddKey(sb, "current_preset", "1");
-            AddKey(sb, "account_list", "");
-            AddKey(sb, "current_comboBox_index_preset_1", "0;0;0");
-            AddKey(sb, "current_comboBox_index_preset_2", "0;0;0");
-            AddKey(sb, "current_comboBox_index_preset_3", "0;0;0");
-            AddKey(sb, "current_comboBox_index_preset_4", "0;0;0");
-            AddKey(sb, "is_test_server", "False");
-            AddKey(sb, "client_path_1", "");
-            AddKey(sb, "client_path_2", "");
-            AddKey(sb, "client_path_3", "");
-            AddKey(sb, "client_path_test_1", "");
-            AddKey(sb, "client_path_test_2", "");
-            AddKey(sb, "client_path_test_3", "");
-            AddKey(sb, "shortcut_name", "거상홈페이지;홈페이지2;홈페이지3;홈페이지4;");
-            AddKey(sb, "shortcut_1", "https://www.gersang.co.kr/main/index.gs");
-            AddKey(sb, "shortcut_2", "");
-            AddKey(sb, "shortcut_3", "");
-            AddKey(sb, "shortcut_4", "");
-            AddKey(sb, "directory_name_client_2", "Gersang2");
-            AddKey(sb, "directory_name_client_3", "Gersang3");
-            AddKey(sb, "directory_name_client_test_2", "GerTest2");
-            AddKey(sb, "directory_name_client_test_3", "GerTest3");
-            AddKey(sb, "is_auto_update", "True");
+            foreach (var item in keyValuePairs) { AddKey(sb, item.Key, item.Value); }
 
             sb.AppendLine("</appSettings>");
             sb.AppendLine("</configuration>");
