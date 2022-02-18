19158d369ef66e5fb8fb500ae3c637ae17255792
byungmeo <wlsrlfvkr@gmail.com>
2022-02-18 10:44:42 +0900

    고급설정창의 저장 버튼을 클릭 시 폼이 닫히도록 수정

--- GersangStation/Form_AdvancedSetting.cs
+++ GersangStation/Form_AdvancedSetting.cs
@@ -13,6 +13,8 @@ namespace GersangStation {
         private void materialButton_save_Click(object sender, EventArgs e) {
             Logger.Log("Click : (" + this.Name + ") " + materialButton_save.Name);
             ConfigManager.setConfig("use_bat_creator", materialCheckbox_useBAT.Checked.ToString());
+
+            this.DialogResult = DialogResult.OK;
         }
 
         private void materialCheckbox_useBAT_CheckedChanged(object sender, EventArgs e) {
