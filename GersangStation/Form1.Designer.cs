af4ed3517b8ac471df658e4c894bd9b1f993c2de
byungmeo <wlsrlfvkr@gmail.com>
2022-02-18 13:59:39 +0900

    [hotfix] 작업표시줄 및 트레이 아이콘 수정

--- GersangStation/Form1.Designer.cs
+++ GersangStation/Form1.Designer.cs
@@ -624,7 +624,7 @@
             this.tabPage_settings.Location = new System.Drawing.Point(4, 31);
             this.tabPage_settings.Name = "tabPage_settings";
             this.tabPage_settings.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_settings.Size = new System.Drawing.Size(394, 226);
+            this.tabPage_settings.Size = new System.Drawing.Size(394, 218);
             this.tabPage_settings.TabIndex = 1;
             this.tabPage_settings.Text = "환경설정";
             // 
@@ -731,7 +731,7 @@
             this.tabPage_help.Location = new System.Drawing.Point(4, 31);
             this.tabPage_help.Name = "tabPage_help";
             this.tabPage_help.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_help.Size = new System.Drawing.Size(394, 226);
+            this.tabPage_help.Size = new System.Drawing.Size(394, 218);
             this.tabPage_help.TabIndex = 2;
             this.tabPage_help.Text = "도움말";
             this.tabPage_help.UseVisualStyleBackColor = true;
@@ -774,7 +774,7 @@
             this.tabPage_info.Location = new System.Drawing.Point(4, 31);
             this.tabPage_info.Name = "tabPage_info";
             this.tabPage_info.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_info.Size = new System.Drawing.Size(394, 226);
+            this.tabPage_info.Size = new System.Drawing.Size(394, 218);
             this.tabPage_info.TabIndex = 4;
             this.tabPage_info.Text = "프로그램 정보";
             this.tabPage_info.UseVisualStyleBackColor = true;
