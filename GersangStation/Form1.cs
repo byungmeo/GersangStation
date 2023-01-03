2ca54109a4520d976f51effadacda4ba20eb827b
byungmeo <wlsrlfvkr@gmail.com>
2023-01-03 19:51:43 +0900

    새로운 공지사항이 게시되면 메시지 출력

--- GersangStation/Form1.cs
+++ GersangStation/Form1.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
+using File = System.IO.File;
 
 namespace GersangStation {
     public partial class Form1 : MaterialForm {
@@ -142,15 +143,27 @@ namespace GersangStation {
                     linkLabel_announcement.Text = "공지사항이 없습니다";
                 } else {
                     string latestAnnouncement = announcements[1];
-                    linkLabel_announcement.Text = latestAnnouncement.Split('{')[0];
+                    string title = latestAnnouncement.Split('{')[0];
+                    int startIndex = latestAnnouncement.LastIndexOf('{') + 1;
+                    int length = latestAnnouncement.LastIndexOf('}') - startIndex;
+                    string pageNumber = latestAnnouncement.Substring(startIndex, length);
+                    string url = "https://github.com/byungmeo/GersangStation/discussions/" + pageNumber;
+                    linkLabel_announcement.Text = title;
                     linkLabel_announcement.Click += (sender, e) => {
-                        
-                        int startIndex = latestAnnouncement.LastIndexOf('{') + 1;
-                        int length = latestAnnouncement.LastIndexOf('}') - startIndex;
-                        string announcementPage = latestAnnouncement.Substring(startIndex, length);
-                        Trace.Write(announcementPage + "번 공지사항 접속");
-                        Process.Start(new ProcessStartInfo("https://github.com/byungmeo/GersangStation/discussions/" + announcementPage) { UseShellExecute = true });
+                        Trace.Write(pageNumber + "번 공지사항 접속");
+                        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
                     };
+
+                    // 만약 이전과 다른 공지사항이 새롭게 게시되었다면 사용자에게 메시지를 출력합니다.
+                    string prevLink = ConfigManager.getConfig("prev_announcement");
+                    if (prevLink == "" || prevLink != url) {
+                        ConfigManager.setConfig("prev_announcement", url);
+                        DialogResult dr = MessageBox.Show($"새로운 공지사항이 게시되었습니다.\n공지제목 :{title.Substring(title.LastIndexOf(']')+1)}\n확인하시겠습니까?", "새로운 공지사항", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if (dr == DialogResult.Yes) {
+                            Trace.Write(pageNumber + "번 공지사항 접속");
+                            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                        }
+                    }
                 }
             } catch (Exception) {
                 linkLabel_announcement.Text = "공지사항을 불러오는데 실패하였습니다";
