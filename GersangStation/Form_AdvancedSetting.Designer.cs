5df0455ee7f67dff6e04aa61b1d79e23009f663e
byungmeo <wlsrlfvkr@gmail.com>
2023-10-04 22:00:03 +0900

    고급 설정 - 마우스 가두기 체크박스의 위치를 조정

--- GersangStation/Form_AdvancedSetting.Designer.cs
+++ GersangStation/Form_AdvancedSetting.Designer.cs
@@ -26,8 +26,7 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             materialCheckbox_useBAT = new MaterialSkin.Controls.MaterialCheckbox();
             mouseClipCheckBox = new MaterialSkin.Controls.MaterialCheckbox();
             SuspendLayout();
@@ -36,7 +35,7 @@
             // 
             materialCheckbox_useBAT.AutoSize = true;
             materialCheckbox_useBAT.Depth = 0;
-            materialCheckbox_useBAT.Location = new Point(74, 181);
+            materialCheckbox_useBAT.Location = new Point(37, 125);
             materialCheckbox_useBAT.Margin = new Padding(0);
             materialCheckbox_useBAT.MouseLocation = new Point(-1, -1);
             materialCheckbox_useBAT.MouseState = MaterialSkin.MouseState.HOVER;
@@ -54,7 +53,7 @@
             // 
             mouseClipCheckBox.AutoSize = true;
             mouseClipCheckBox.Depth = 0;
-            mouseClipCheckBox.Location = new Point(74, 237);
+            mouseClipCheckBox.Location = new Point(37, 85);
             mouseClipCheckBox.Margin = new Padding(0);
             mouseClipCheckBox.MouseLocation = new Point(-1, -1);
             mouseClipCheckBox.MouseState = MaterialSkin.MouseState.HOVER;
@@ -70,16 +69,14 @@
             // 
             // Form_AdvancedSetting
             // 
-            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(798, 416);
+            ClientSize = new Size(399, 195);
             Controls.Add(mouseClipCheckBox);
             Controls.Add(materialCheckbox_useBAT);
-            Margin = new Padding(6);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Form_AdvancedSetting";
-            Padding = new Padding(6, 137, 6, 6);
             ShowInTaskbar = false;
             Sizable = false;
             StartPosition = FormStartPosition.CenterParent;
