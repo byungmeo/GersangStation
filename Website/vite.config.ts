6d0efd90e4d77c2e617c651c66f40c107096020b
jeheecheon <jeheecheon@gmail.com>
2024-06-24 00:25:20 +0900

    chore: tailwindcss, vite plugin 이외 부가 모듈 설치 및 세팅

--- Website/vite.config.ts
+++ Website/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/app"),
+    },
+  },
+});
