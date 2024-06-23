69c75e5b51c249c21d863147c204d55737a6ce3e
jeheecheon <jeheecheon@gmail.com>
2024-06-24 06:50:30 +0900

    fix: assets 404 error

--- Website/vite.config.ts
+++ Website/vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/GersangStation",
   plugins: [svgr(), react()],
   resolve: {
     alias: {
