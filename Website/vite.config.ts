4fbd15b2d32a2a061c09cfd2028080c518dc4f45
jeheecheon <jeheecheon@gmail.com>
2024-06-24 06:46:56 +0900

    bug: assets 404에러

--- Website/vite.config.ts
+++ Website/vite.config.ts
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/GersangStation",
   plugins: [svgr(), react()],
   resolve: {
     alias: {
