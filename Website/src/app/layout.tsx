702313879e29a1a8d2a96915a6568cadc7c1f8bd
byungmeo <wlsrlfvkr@gmail.com>
2024-06-24 20:01:31 +0900

    레이아웃 수정

--- Website/src/app/layout.tsx
+++ Website/src/app/layout.tsx
@@ -1,26 +1,26 @@
 import { Outlet } from "react-router-dom";
 
-import Tag from "./_components/Tag";
+// import Tag from "./_components/Tag";
 
 import background from "@/_assets/images/background.png";
 import imageTitleRound from "@/_assets/images/image_title_round.png";
-import handImage from "@/_assets/images/icon_hand.png";
+// import handImage from "@/_assets/images/icon_hand.png";
 
 const exLinks: {
   title: string;
   link: string;
 }[] = [
   {
-    title: "사용설명서",
-    link: "",
+    title: "설치/사용설명서",
+    link: "https://github.com/byungmeo/GersangStation/wiki/%EC%82%AC%EC%9A%A9%EC%9E%90-%EC%84%A4%EB%AA%85%EC%84%9C",
   },
   {
-    title: ".NET",
-    link: "",
+    title: ".NET SDK",
+    link: "https://dotnet.microsoft.com/en-us/download/dotnet/6.0",
   },
   {
-    title: "Edge",
-    link: "",
+    title: "Edge Beta",
+    link: "https://go.microsoft.com/fwlink/?linkid=2100017&Channel=Beta&language=ko&brand=M103",
   },
   {
     title: "원격지원",
@@ -43,21 +43,21 @@ function SupportLayout() {
         <div className="px-4 lg:p-0 pb-4 lg:pb-0 pt-3 lg:pt-0">
           <div className="lg:fixed h-full flex flex-col justify-center">
             <div className="font-semibold leading-[55px] lg:leading-[85px] text-[35px] lg:text-[60px] ">
-              <div>
+              <p>다클 생성, 패치</p>
+              <p>3분만에!</p>
+              <div className="lg:mt-8">
                 <img
                   src={imageTitleRound}
                   alt="토닥토닥"
-                  className="absolute w-[130px] lg:w-[224px] -translate-y-1 lg:-translate-y-3 mx-auto z-[-1]"
+                  className="absolute w-[260px] lg:w-[448px] -translate-y-1 lg:-translate-y-3 mx-auto z-[-1]"
                 />
-                천하제일
+                <p className="text-[#6151f3]">거상 스테이션</p>
               </div>
-              <p className="lg:mt-8">사용 방법</p>
-              <p className="text-[#6151f3]">거상 스테이션</p>
             </div>
 
-            <button className="hidden lg:block mt-8 bg-[#6151f3] rounded-full text-xl font-bold text-white px-7 py-4">
+            <a className="hidden lg:block mt-8 bg-[#6151f3] rounded-full text-xl font-bold text-white text-center px-7 py-4" href="https://github.com/byungmeo/GersangStation/releases/latest" target="_blank">
               설치하기
-            </button>
+            </a>
 
             <ul className="hidden grid-cols-2 gap-3 mt-5 lg:grid">
               {exLinks.map((link, index) => (
@@ -73,7 +73,8 @@ function SupportLayout() {
                 </li>
               ))}
             </ul>
-
+            
+{/*
             <div className="space-y-3 mt-[30px]">
               <div className="flex items-center gap-2">
                 <p className="text-xl font-bold">사용방법이 궁금하신가요?</p>
@@ -85,6 +86,7 @@ function SupportLayout() {
                 <Tag text="1분만에" />
               </div>
             </div>
+*/}
           </div>
         </div>
 
