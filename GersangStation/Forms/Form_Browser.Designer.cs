48f8be3f516748c75a7570f45eadd5d00497af5c
byungmeo <wlsrlfvkr@gmail.com>
2026-02-03 20:48:32 +0900

    webview2 브라우저 버튼 텍스트 수정

--- GersangStation/Forms/Form_Browser.Designer.cs
+++ GersangStation/Forms/Form_Browser.Designer.cs
@@ -23,52 +23,52 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.addressBar = new System.Windows.Forms.TextBox();
-            this.goButton = new System.Windows.Forms.Button();
-            this.button_saveShortcut = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            addressBar = new TextBox();
+            goButton = new Button();
+            button_saveShortcut = new Button();
+            SuspendLayout();
             // 
             // addressBar
             // 
-            this.addressBar.Location = new System.Drawing.Point(9, 9);
-            this.addressBar.Margin = new System.Windows.Forms.Padding(0);
-            this.addressBar.Name = "addressBar";
-            this.addressBar.Size = new System.Drawing.Size(872, 23);
-            this.addressBar.TabIndex = 0;
-            this.addressBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.addressBar_KeyDown);
+            addressBar.Location = new Point(9, 9);
+            addressBar.Margin = new Padding(0);
+            addressBar.Name = "addressBar";
+            addressBar.Size = new Size(872, 23);
+            addressBar.TabIndex = 0;
+            addressBar.KeyDown += addressBar_KeyDown;
             // 
             // goButton
             // 
-            this.goButton.Location = new System.Drawing.Point(884, 9);
-            this.goButton.Name = "goButton";
-            this.goButton.Size = new System.Drawing.Size(75, 23);
-            this.goButton.TabIndex = 1;
-            this.goButton.Text = "Go!";
-            this.goButton.UseVisualStyleBackColor = true;
-            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            goButton.Location = new Point(884, 9);
+            goButton.Name = "goButton";
+            goButton.Size = new Size(75, 23);
+            goButton.TabIndex = 1;
+            goButton.Text = "이동";
+            goButton.UseVisualStyleBackColor = true;
+            goButton.Click += goButton_Click;
             // 
             // button_saveShortcut
             // 
-            this.button_saveShortcut.Location = new System.Drawing.Point(965, 9);
-            this.button_saveShortcut.Name = "button_saveShortcut";
-            this.button_saveShortcut.Size = new System.Drawing.Size(127, 23);
-            this.button_saveShortcut.TabIndex = 2;
-            this.button_saveShortcut.Text = "나만의바로가기 저장";
-            this.button_saveShortcut.UseVisualStyleBackColor = true;
-            this.button_saveShortcut.Click += new System.EventHandler(this.button_saveShortcut_Click);
+            button_saveShortcut.Location = new Point(965, 9);
+            button_saveShortcut.Name = "button_saveShortcut";
+            button_saveShortcut.Size = new Size(127, 23);
+            button_saveShortcut.TabIndex = 2;
+            button_saveShortcut.Text = "나만의바로가기 저장";
+            button_saveShortcut.UseVisualStyleBackColor = true;
+            button_saveShortcut.Click += button_saveShortcut_Click;
             // 
             // Form_Browser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1097, 761);
-            this.Controls.Add(this.button_saveShortcut);
-            this.Controls.Add(this.goButton);
-            this.Controls.Add(this.addressBar);
-            this.Name = "Form_Browser";
-            this.Resize += new System.EventHandler(this.Form_Browser_Resize);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1097, 761);
+            Controls.Add(button_saveShortcut);
+            Controls.Add(goButton);
+            Controls.Add(addressBar);
+            Name = "Form_Browser";
+            Resize += Form_Browser_Resize;
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
